183ba05f1e5c53be198a3914fb01f3810b9250ae
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-05-17 10:13:59 +0100

    Update prelim.R

--- prelim.R
+++ prelim.R
@@ -13,37 +13,25 @@ n <- 1000
 # define semivariogram, locations (x), and simulate data (y)
 
 svar <- function(x1_x2, theta = c(0.2, 1.8))
-  
   (norm(x1_x2, type = "2") / theta[1])^theta[2]
 
 x <- expand.grid(1:5, 1:2)
-
 K <- dim(x)[1]
-
 y <- simulPareto(n, x, svar)
-
 sums <- sapply(y, sum)
-
 exceedances <- y[sums > quantile(sums, 0.9)]
 
 
 
 # negative log-likelihood
-
 nllh <- function(theta) {
-  
   aux <- function(x1_x2)
-    
     svar(x1_x2, theta)
   
   # impose parametric constraints via penalization
-
   ifelse(theta[1] > 0 & theta[2] > 0 & theta[2] < 2,
-
          spectralLikelihood(exceedances, x, aux)[1],
-
          10^10)
-
 }
 
  
